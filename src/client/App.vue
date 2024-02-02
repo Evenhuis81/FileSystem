e4f83bcdd991b31e5a00b6378d8d39888220f39f
EvenhuisD <devenhuis81@gmail.com>
2024-02-02 11:43:17 +0100

    first commit

---
+++ src/client/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Filesystem</h1>
+  <hr />
+</template>
+
+<script setup lang="ts">
+console.log("app.vue loaded");
+</script>
---
+++ src/client/main.ts
@@ -0,0 +1,8 @@
+// import "./style.css";
+import "./style_reset.css";
+
+import { createApp } from "vue";
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
