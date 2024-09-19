3651e718ed458c888eff3d58d4ae9e03a85ad358
EvenhuisD <devenhuis81@gmail.com>
2024-09-19 23:19:51 +0200

    Forum -> succesfull link to post request axios website scraper gallerydutchart

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,23 +1,38 @@
 <template>
   <h1>Filesystem</h1>
   <hr />
+  <button @click="scrapeThis">Scrape Test</button>
 </template>
 
 <script setup lang="ts">
 import { onMounted, reactive } from "vue";
 import axios from "axios";
 
-type FileSys = {
-  path: string;
-  content: Array<{}>;
+// type FileSys = {
+//   path: string;
+//   content: Array<{}>;
+// };
+
+const path = "/scraper/test";
+
+// const fileSys: FileSys = reactive({
+//   path: "c:\\",
+//   content: [],
+// });
+
+const scrapeThis = async () => {
+  console.log("clicked");
+
+  const dat = await axios.post("/scrape", { path });
+
+  console.log(dat);
+
+  // https://file.io/0qi3d8iPorIc
 };
 
-const fileSys: FileSys = reactive({
-  path: "c:\\",
-  content: [],
-});
-
 onMounted(async () => {
-  const dat = await axios.post<FileSys>("/fs", { path: fileSys.path });
+  // const dat = await axios.post<FileSys>("/fs", { path: fileSys.path });
+  // const dat = await axios.post("/fs", { path: fileSys.path });
+  // console.log(dat);
 });
 </script>
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,28 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import scrape from 'website-scraper'; // only as ESM, no CommonJS
+
+const options = {
+  urls: ['https://www.gallerydutchart.nl/'],
+  directory: '/scraper/test2',
+};
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/scrape', async (req, res) => {
+
+  // console.log(req.body);
+
+  
+// with async/await
+  const result = await scrape(options);
+
+  // console.log(result);
+
+  res.send('done');
+})
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + Vue + TypeScript!");
@@ -15,7 +34,7 @@ app.post('/fs', (req, res) => {
   console.log(body);
 
   res.send('test');
-})
+});
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
